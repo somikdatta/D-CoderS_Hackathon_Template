433e64b3e840199111154d6fb527a5b48cc22c6b
Somik Datta <developersomik@gmail.com>
2020-02-09 11:03:00 +0530

    models updated

---
+++ Application Code/backend/middleware/check-privilege.js
@@ -0,0 +1,21 @@
+exports.teacher = (req, res, next) => {
+    if (parseInt(req.userData.authorization !== 1)) {
+        return res.status(401).json({
+            message: 'You do not have the required authorization'
+        })
+    }
+    else {
+        next();
+    }
+}
+
+exports.hod = (req, res, next) => {
+    if (parseInt(req.userData.authorization !== 0)) {
+        return res.status(401).json({
+            message: 'You do not have the required authorization'
+        })
+    }
+    else {
+        next();
+    }
+}
