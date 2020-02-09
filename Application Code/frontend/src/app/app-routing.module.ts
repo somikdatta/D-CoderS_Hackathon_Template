69cb131145b9a14a0f196bc4aa1fe91b7843add3
Somik Datta <developersomik@gmail.com>
2020-02-09 14:49:39 +0530

    reverse-guard, routes

--- Application Code/frontend/src/app/app-routing.module.ts
+++ Application Code/frontend/src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { SignupComponent } from "./auth/signup/signup.component";
 import { LoginComponent } from "./auth/login/login.component";
+import { ReverseGuard } from "./auth/reverse.guard";
 
 const routes: Routes = [
-  { path: "signup", component: SignupComponent },
-  { path: "login", component: LoginComponent }
+  { path: "signup", component: SignupComponent, canActivate: [ReverseGuard] },
+  { path: "login", component: LoginComponent, canActivate: [ReverseGuard] }
 ];
 
 @NgModule({
